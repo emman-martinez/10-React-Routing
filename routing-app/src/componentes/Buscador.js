1f8402eac639fc144ee260a019d17ce2fc615527
Emasesosos <j.emmanuel.martinez.e@hotmail.com>
2019-05-22 20:19:38 -0500

    Creación barra de Navegación, Secciones Contacto y Nosotros, NavLink

---
+++ routing-app/src/componentes/Buscador.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Buscador.css';
+
+class Buscador extends Component {
+    render() {
+        return(
+            
+        );
+    }
+}
+
+export default Buscador;
--- routing-app/src/componentes/Router.js
+++ routing-app/src/componentes/Router.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './Header';
+import Navegacion from './Navegacion';
 import Productos from './Productos';
 import Nosotros from './Nosotros';
+import Contacto from './Contacto';
 import SingleProducto from './SingleProducto';
 import Error from './Error';
 import infoProductos from './../datos/datos.json';
@@ -24,23 +26,36 @@ class Router extends Component {
             <BrowserRouter>
                 { /* Componente: Header */ }
                 <Header></Header>
+                { /* Componente: Navegacion */ }
+                <Navegacion></Navegacion>
                 <Switch>
                     <Route exact path="/" render={() => (
+                         /* Componente: Productos */
                         <Productos
                                     productos={this.state.productos}
                         ></Productos>
                     )}></Route>
+                    {/* Componente: Nosotros */}
                     <Route exact path="/nosotros" component={Nosotros}></Route>
+                    <Route exact path="/productos" render={() => (
+                        <Productos
+                                    productos={this.state.productos}
+                        ></Productos>
+                    )}></Route>
                     <Route exact path="/producto/:productoId" render={(props) => {
                         // console.log(props);
                         let idProducto = props.location.pathname.replace('/producto/', '');
                         // console.log(idProducto);
                         return(
+                            /* Componente: SingleProducto */
                             <SingleProducto
                                             producto={this.state.productos[idProducto]}
                             ></SingleProducto>
                         )
                     }}></Route>
+                    { /* Componente: Contacto */}
+                    <Route exact path="/contacto" component={Contacto}></Route>
+                    { /* Componente: Error */}
                     <Route component={Error}></Route>
                 </Switch> 
             </BrowserRouter>
